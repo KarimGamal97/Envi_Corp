a570523436a2a1e302c99ee73a7860ed438df5a3
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-07-16 02:52:51 +0300

    update consultation

--- components/sections/Home8/Section9.vue
+++ components/sections/Home8/Section9.vue
@@ -1,6 +1,6 @@
 <template>
     <section
-        class="relative bg-[url('/assets/images/main-imgs/consult.jpg')] bg-cover bg-center bg-fixed flex justify-center items-center px-10 md:px-20 py-40 md:py-80"
+        class="relative bg-[url('/assets/images/main-imgs/consult.jpg')] bg-cover bg-center bg-fixed flex justify-center items-center px-10 md:px-20 h-screen"
     >
         <div class="absolute inset-0 bg-black opacity-50 z-1"></div>
         <div class="text-center z-10">
