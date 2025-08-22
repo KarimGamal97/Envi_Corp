6cc98f331dfbd7724b6a99efed8bb2a72cc0d25a
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-08-22 07:30:24 +0300

    create sql page

--- pages/services/list-of-applications.vue
+++ pages/services/list-of-applications.vue
@@ -158,7 +158,7 @@
                     </div>
                 </NuxtLink>
                 <!-- item 9  -->
-                <NuxtLink class="relative group" to="/coming-soon">
+                <NuxtLink class="relative group" to="/services/small-quantity-importation">
                     <div
                         class="absolute inset-0 w-full h-full opacity-100 z-10 group-hover:opacity-0 group-active:opacity-100 group-active:bg-[#86B93F] transition-all duration-300 bg-[#547326] py-8 px-10 rounded-lg flex items-center justify-center"
                     >
---
+++ pages/services/small-quantity-importation.vue
@@ -0,0 +1,90 @@
+<template>
+    <!-- hero section  -->
+    <section
+        class="bg-[url('/assets/images/main-imgs/services/application-img.jpg')] bg-cover bg-left md:bg-top min-h-dvh py-20 w-full px-10 md:px-20 flex items-center"
+        id="top"
+    >
+        <div class="md:max-w-[45%] text-white">
+            <!-- text 1 -->
+            <h1 class="text-2xl md:text-5xl 4k:text-6xl mb-5">
+                Small Quantity Importation (SQI) Clearance
+            </h1>
+            <p class="text-lg 4k:text-3xl">
+                A DENR Small Quantity Importation Clearance permits importing limited amounts of
+                chemical substances not yet on the Philippine Inventory, for specific, controlled
+                uses.
+            </p>
+            <div class="flex gap-2 mt-5 w-full flex-wrap">
+                <NuxtLink
+                    to="#tsd-new"
+                    class="4k:text-2xl border-2 border-white transition-all duration-200 text-white py-2 px-5 rounded-lg uppercase"
+                >
+                    New
+                </NuxtLink>
+            </div>
+        </div>
+    </section>
+    <!-- body  -->
+    <section class="py-10 md:py-20">
+        <div class="container">
+            <!-- section 1  -->
+            <section id="tsd-new" class="py-10">
+                <h2 class="text-2xl md:text-4xl font-regular text-center mb-5 uppercase">
+                    Small Quantity Importation (SQI) Clearance
+                </h2>
+
+                <h4
+                    class="uppercase bg-[#547326] py-1 text-xl md:text-2xl 4k:text-3xl text-center rounded text-white font-semibold mb-10"
+                >
+                    scope OF WORK - NEW
+                </h4>
+                <h5 class="text-lg md:text-xl mb-10 font-semibold">
+                    RESPONSIBILITIES OF THE CONSULTANT
+                </h5>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li class="mb-2">Processing of Online Application</li>
+                    <li class="mb-2">Notary of Duly Accomplished Application Form</li>
+                </ul>
+                <h4
+                    class="uppercase bg-[#547326] py-1 text-xl md:text-2xl 4k:text-3xl text-center rounded text-white font-semibold mb-10"
+                >
+                    REQUIREMENTS - nEW
+                </h4>
+                <h5 class="text-lg md:text-xl mb-10 font-semibold">REQUIREMENT FROM PROPONENT</h5>
+                <ul class="4k:text-2xl list-disc list-inside mb-10 ml-[19px]">
+                    <li class="mb-2">Provision of Material Safety Data Sheet of the substance</li>
+                    <li class="mb-2">
+                        Provision of Business Permit or SEC Registration of the Office
+                    </li>
+                    <li class="mb-2">
+                        Provision of Business Permit or SEC Registration of the Warehouse/Storage
+                        Facility of the substance
+                    </li>
+                    <li class="mb-2">Provision of Bill of Lading</li>
+                </ul>
+                <div class="flex flex-col gap-3 items-start justify-center">
+                    <NuxtLink
+                        to="#"
+                        class="flex-1 4k:text-2xl bg-[#547326] transition-all duration-200 text-white py-2 px-5 rounded"
+                    >
+                        Download PDF
+                    </NuxtLink>
+                    <NuxtLink
+                        to="#top"
+                        class="flex-1 4k:text-2xl border-2 border-[#547326] transition-all duration-200 text-[#547326] py-2 px-5 rounded"
+                    >
+                        Back to top
+                    </NuxtLink>
+                </div>
+            </section>
+        </div>
+    </section>
+</template>
+
+<script setup>
+useHead({
+    title: "Small Quantity Importation (SQI) Clearance - Envi-Comm Corporation",
+});
+</script>
+
+<style lang="scss" scoped></style>
